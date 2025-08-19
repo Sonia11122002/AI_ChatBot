2a8128abb794b3a239439af353972250db0e691d
Sonia11122002 <sonia32248536@gmail.com>
2025-08-19 14:25:07 +0530

    first commit

---
+++ langchain_cb.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+llmmodel = HuggingFaceEndpoint(
+    repo_id='meta-llama/Llama-3.1-8B-Instruct',
+    task='text-generation',
+)
+
+model = ChatHuggingFace(llm=llmmodel)
+result = model.invoke("Capital of Japan?")
+print(result.content)
